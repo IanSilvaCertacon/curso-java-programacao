3b41e7b26b3f0076a972ef6bed9f436da18d5eac
IanSilvaCertacon <ian.silva@certacon.com.br>
2024-11-12 11:03:42 -0300

    static

--- src/OriObj/Main.java
+++ src/OriObj/Main.java
@@ -2,7 +2,7 @@ package OriObj;
 
 public class Main {
     public static void main(String[] args) {
-        Casa casa1 = new Casa(false);
+       /* Casa casa1 = new Casa(false);
         Casa casa2 = new Casa(true);
         Casa casa3 = new Casa();
 
@@ -12,6 +12,13 @@ public class Main {
         casa2.acenderLuz(0);
         casa3.acenderLuz(0);
 
-        System.out.println(casa3.nome);
+        casa3.nome = "Sua casa";
+        System.out.println(casa3.nome);*/
+
+        Double soma = Matematica.sum(1.0, 1.0);
+        Double somaNaoEstatica = new Matematica().somaNaoEstatica(1.0, 1.0);
+        System.out.println(Matematica.limite);
+        System.out.println(somaNaoEstatica);
+        System.out.println(soma);
     }
 }
